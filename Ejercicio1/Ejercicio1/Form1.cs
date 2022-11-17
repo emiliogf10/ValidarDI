85dac97e9d4afe8dc4384e959125da209bdd29f5
emiliogf10 <113925988+emiliogf10@users.noreply.github.com>
2022-11-17 19:20:04 +0100

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,142 @@
+#define TECLA
+using Microsoft.VisualBasic.Devices;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Mouse Tester";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mover(object sender, MouseEventArgs e)
+        {
+
+
+
+
+            if (sender == button1)
+            {
+            
+
+                Text = String.Format(" Mouse Tester X:{0}, Y:{1} ", button1.Location.X + e.Location.X, button1.Location.Y + e.Location.Y);
+
+            }
+            else if (sender == button2)
+            {
+
+
+                Text = String.Format(" Mouse Tester X:{0}, Y:{1} ", (button2.Location.X + e.Location.X), (button2.Location.Y + e.Location.Y));
+            }
+            else
+            {
+                Text = String.Format("Mouse Tester X:{0}, Y:{1} ", e.Location.X, e.Location.Y);
+            }
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+
+            Text = "Mouse Tester";
+
+
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                button1.BackColor = Color.Green;
+
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                button2.BackColor = Color.Red;
+            }
+            else
+            {
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Red;
+
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                button1.BackColor = Color.White;
+
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                button2.BackColor = Color.White;
+            }
+            else
+            {
+                button1.BackColor = Color.White;
+                button2.BackColor = Color.White;
+
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+#if TECLAS
+
+
+            this.Text = String.Format("Letra pulsada:{0}", e.KeyValue.ToString());
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Text = "Mouse Tester";
+            }
+
+            //Text = String.Format("La letra es:{0}", e.KeyCode);
+#endif
+        }
+
+
+        private void Form1_mantener(object sender, KeyPressEventArgs e)
+        {
+#if !TECLAS
+            this.Text = String.Format("El caracter pulsado es: {0}",e.KeyChar.ToString());
+
+            if (e.KeyChar.Equals("Esc"))
+            {
+                this.Text = "Mouse Tester";
+            }
+
+#endif
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que desea salir?", "Mi Aplicación",
+           MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+            == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.botonSalir = new System.Windows.Forms.Button();
+            this.color1 = new System.Windows.Forms.TextBox();
+            this.color3 = new System.Windows.Forms.TextBox();
+            this.color2 = new System.Windows.Forms.TextBox();
+            this.botonColor = new System.Windows.Forms.Button();
+            this.botonImagen = new System.Windows.Forms.Button();
+            this.etiquetaImagen = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // botonSalir
+            // 
+            this.botonSalir.Location = new System.Drawing.Point(228, 392);
+            this.botonSalir.Name = "botonSalir";
+            this.botonSalir.Size = new System.Drawing.Size(332, 23);
+            this.botonSalir.TabIndex = 6;
+            this.botonSalir.Text = "Salir";
+            this.botonSalir.UseVisualStyleBackColor = true;
+            this.botonSalir.Click += new System.EventHandler(this.botonSalir_Click);
+            this.botonSalir.MouseLeave += new System.EventHandler(this.salir);
+            this.botonSalir.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // color1
+            // 
+            this.color1.Location = new System.Drawing.Point(34, 91);
+            this.color1.Name = "color1";
+            this.color1.Size = new System.Drawing.Size(134, 23);
+            this.color1.TabIndex = 0;
+            this.color1.Click += new System.EventHandler(this.cargar);
+            this.color1.Enter += new System.EventHandler(this.color1_Enter);
+            // 
+            // color3
+            // 
+            this.color3.Location = new System.Drawing.Point(374, 91);
+            this.color3.Name = "color3";
+            this.color3.Size = new System.Drawing.Size(155, 23);
+            this.color3.TabIndex = 2;
+            this.color3.Click += new System.EventHandler(this.cargar);
+            this.color3.Enter += new System.EventHandler(this.color3_Enter);
+            // 
+            // color2
+            // 
+            this.color2.Location = new System.Drawing.Point(206, 91);
+            this.color2.Name = "color2";
+            this.color2.Size = new System.Drawing.Size(143, 23);
+            this.color2.TabIndex = 1;
+            this.color2.Click += new System.EventHandler(this.cargar);
+            this.color2.Enter += new System.EventHandler(this.color2_Enter);
+            // 
+            // botonColor
+            // 
+            this.botonColor.Location = new System.Drawing.Point(592, 89);
+            this.botonColor.Name = "botonColor";
+            this.botonColor.Size = new System.Drawing.Size(114, 23);
+            this.botonColor.TabIndex = 3;
+            this.botonColor.Text = "Color";
+            this.botonColor.UseVisualStyleBackColor = true;
+            this.botonColor.Click += new System.EventHandler(this.botonColor_Click);
+            this.botonColor.MouseLeave += new System.EventHandler(this.salir);
+            this.botonColor.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // botonImagen
+            // 
+            this.botonImagen.Location = new System.Drawing.Point(207, 201);
+            this.botonImagen.Name = "botonImagen";
+            this.botonImagen.Size = new System.Drawing.Size(142, 23);
+            this.botonImagen.TabIndex = 5;
+            this.botonImagen.Text = "Cargar imagen";
+            this.botonImagen.UseVisualStyleBackColor = true;
+            this.botonImagen.Click += new System.EventHandler(this.botonImagen_Click);
+            this.botonImagen.MouseLeave += new System.EventHandler(this.salir);
+            this.botonImagen.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // etiquetaImagen
+            // 
+            this.etiquetaImagen.AutoSize = true;
+            this.etiquetaImagen.Location = new System.Drawing.Point(435, 187);
+            this.etiquetaImagen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.etiquetaImagen.Name = "etiquetaImagen";
+            this.etiquetaImagen.Size = new System.Drawing.Size(0, 15);
+            this.etiquetaImagen.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(374, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Click += new System.EventHandler(this.cargar);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ruta:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.etiquetaImagen);
+            this.Controls.Add(this.botonImagen);
+            this.Controls.Add(this.botonColor);
+            this.Controls.Add(this.color2);
+            this.Controls.Add(this.color3);
+            this.Controls.Add(this.color1);
+            this.Controls.Add(this.botonSalir);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.cargar);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button botonSalir;
+        private TextBox color1;
+        private TextBox color3;
+        private TextBox color2;
+        private Button botonColor;
+        private Button botonImagen;
+        private Label etiquetaImagen;
+        private TextBox textoImagen;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
